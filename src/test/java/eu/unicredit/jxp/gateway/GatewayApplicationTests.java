e82b74f4d5ce5f0fed64ddffe95f54240fa5adce
msloma <msloma@gmail.com>
2020-05-01 23:59:01 +0200

    unit tests

--- src/test/java/eu/unicredit/jxp/gateway/GatewayApplicationTests.java
+++ src/test/java/eu/unicredit/jxp/gateway/GatewayApplicationTests.java
@@ -1,13 +1,29 @@
 package eu.unicredit.jxp.gateway;
 
+import eu.unicredit.jxp.gateway.model.Approvers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class GatewayApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getUserids() {
+		Approvers a = new Approvers("u");
+		assertTrue("u".equals(a.getUserids()));
+	}
+
+	@Test
+	void getMails() {
+		Approvers a = new Approvers("u");
+		assertEquals("u@mailasp.internal", a.getMails());
 	}
 
 }
